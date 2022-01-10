fec9e204a2269d06298d667cab45bfd0a7551459
Tatiana Prada <tatiana.1227@hotmail.com>
2022-01-10 17:57:04 +0000

    deploy

--- server/app.js
+++ server/app.js
@@ -11,15 +11,20 @@ const express = require("express");
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, "public"))) // MIDDLEWARE DE CONFIGURACIÓN
+
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+app.use((req, res) => res.sendFile(__dirname + "/public/index.html")); // MIDDLEWARE DE ENVÍO AL HTML
+
+
+
 
 module.exports = app;
